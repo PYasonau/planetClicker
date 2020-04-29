3e9b973c3095ec11b80a77505600c4766f9da418
Pavel Yasonau <megomozg@gmail.com>
2020-04-29 17:37:43 +0300

    small changes according discussion

--- js/main.js
+++ js/main.js
@@ -1,51 +1,77 @@
-$('.shop-menu-btn').click(function(e) {
+
+// загрузка информации при загрузке страницы
+function load(){
+	toggleClass();
+	parralacsEffect();
+	firstLoadInit();
+	
+    //del
+	document.querySelector('.currentClicks').innerHTML = getCurrentClicks();
+	
+	document.querySelector('.score').innerHTML = getScore();
+}
+
+addClicks = () => {
+	var currentClicksNew = getCurrentClicks() + 1;
+	setCurrentClicksLs(currentClicksNew);
+	//del
+	document.querySelector('.currentClicks').innerHTML = currentClicksNew;
+}
+
+// добавление кликов при нажатии на планету
+addScore = () => {
+	console.log('clicked')
+	var scoreNew = getScore() + getCurrentClicks();
+	console.log(scoreNew)
+	setScoreToLs(scoreNew);
+	document.querySelector('.score').innerHTML = scoreNew;
+}
+
+toggleClass = () => {
+	$('.shop-menu-btn').click(function(e) {
   e.preventDefault();
   $('.shop-menu').toggleClass('shop-menu_active');
 });
+}
 
-//$('#responsiveTabsDemo').responsiveTabs({
-//	    startCollapsed: 'accordion'
-//	});
-// для вкладок https://pcvector.net/scripts/tabs/470-adaptivnye-vkladki-responsive-tabs.html
-
-//параллакс эффект
-$(document).on('mousemove', function(e) {
+parralacsEffect = () => {
+	$(document).on('mousemove', function(e) {
             var X = 10 * ((e.pageX + 1) / $(document).width()) - 120;
             var Y = 10 * ((e.pageY + 1) / $(window).height()) - 200;
 
             $('.parallax__layer').css('background-position', X + 'px ' + Y + 'px');
 } ); 
-
-
-let score = 0; // кол-во кликов
-let currentClicks = 1; // текущие нажатия
-
-
-// загрузка информации при загрузке страницы
-function load(){
-	this.score = 0;
-	this.currentClicks = 1;
-	this.score = localStorage.getItem("score");
-	this.score = parseInt(score);
-
-	currentClicks = localStorage.getItem("currentClicks");
-	currentClicks = parseInt(this.currentClicks);
-    //del
-	document.querySelector('.currentClicks').innerHTML = this.currentClicks;
-	
-	document.querySelector('.score').innerHTML = this.score;
 }
-function addClicks(){
-	this.currentClicks = this.currentClicks + 1;
-	localStorage.setItem("currentClicks", this.currentClicks);
-	//del
-	document.querySelector('.currentClicks').innerHTML = this.currentClicks;
+
+getScore =() => {
+	return parseInt(localStorage.getItem("score"));
 }
-// добавление кликов при нажатии на планету
-function addScore(){
-	this.score = this.score + currentClicks;
-	document.querySelector('.score').innerHTML = this.score;
-	
 
+getCurrentClicks =() => {
+	return parseInt(localStorage.getItem("currentClicks"));
+}
 
+setScoreToLs = (score) => {
+	localStorage.setItem("score", score);
+}
 
+setCurrentClicksLs = (currentClicks) => {
+	localStorage.setItem("currentClicks", currentClicks);
+}
+
+isInitiated = () => {
+	return localStorage.getItem("initiated") || false;
+}
+
+setInitiated = () => {
+	localStorage.setItem("initiated", true);
+}
+
+firstLoadInit = () => {
+if(!isInitiated()){
+	setScoreToLs(0);
+	setCurrentClicksLs(0);
+	setInitiated();
+}
+
+}
